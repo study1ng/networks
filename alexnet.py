f872609adba0269bc1686e563fe9fbc83bd7d164
xxx <daisuke912816@icloud.com>
2025-08-25 12:32:21 +0900

    implemented nin

--- alexnet.py
+++ alexnet.py
@@ -1,27 +1,49 @@
+# Krizhevsky, A., Sutskever, I., & Hinton, G. E. (2012). Imagenet classification with deep convolutional neural networks. Advances in neural information processing systems, 25.
+# Implemented AlexNet model
+# In AlexNet, Dropout layer's output is multiplied with 0.5, but in torch, nn.Dropout is inverted so we don't need such technique.
 import torch
 from torch import nn
-import lightning
 
 
 class AlexNet(nn.Module):
     def __init__(self):
         super(AlexNet, self).__init__()
-        self.layers = nn.Sequential([
+        self.pregpu = nn.Sequential(
             nn.Conv2d(3, 96, kernel_size=11, stride=4),
             nn.ReLU(),
             nn.LocalResponseNorm(size=5, alpha=0.0001, beta=0.75, k=2),
             nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(96, 256, kernel_size=5, padding=2),
+        )
+        # AlexNet separates the first conv layer's output to process on different gpu.
+        # Though pytorch implicitly does this, to reproduce the implementation based the original paper, we explicitly separate them.
+        # Actually, this code does not process parallel. Just for imitation.
+        self.gpu1 = nn.Sequential(
+            nn.Conv2d(48, 128, kernel_size=5, padding=2),
             nn.ReLU(),
             nn.LocalResponseNorm(size=5, alpha=0.0001, beta=0.75, k=2),
             nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(256, 384, kernel_size=3, padding=1),
+            nn.Conv2d(128, 192, kernel_size=3, padding=1),
             nn.ReLU(),
-            nn.Conv2d(384, 384, kernel_size=3, padding=1),
+            nn.Conv2d(192, 192, kernel_size=3, padding=1),
             nn.ReLU(),
-            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.Conv2d(192, 128, kernel_size=3, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.gpu2 = nn.Sequential(
+            nn.Conv2d(48, 128, kernel_size=5, padding=2),
+            nn.ReLU(),
+            nn.LocalResponseNorm(size=5, alpha=0.0001, beta=0.75, k=2),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(128, 192, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(192, 192, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(192, 128, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.postgpu = nn.Sequential(
             nn.Flatten(),
             nn.Linear(256 * 6 * 6, 4096),
             nn.ReLU(),
@@ -30,10 +52,24 @@ class AlexNet(nn.Module):
             nn.ReLU(),
             nn.Dropout(p=0.5),
             nn.Linear(4096, 1000),
-        ])
+        )
+        self._initialize_weights()
 
     def forward(self, x):
-        return self.layers(x)
+        x = self.pregpu(x)
+        x1 = self.gpu1(x[:, :48, ...])
+        x2 = self.gpu2(x[:, 48:, ...])
+        x = torch.cat([x1, x2], dim=1)
+        x = self.postgpu(x)
+        return x
+
+    def _initialize_weights(self):
+        # By default, torch initialize weights with Kaiming initialization. To reproduce the original paper, we use normal initialize.
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, mean=0, std=0.01)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
 
 
 net = AlexNet()
--- nin.py
+++ nin.py
@@ -1,14 +1,40 @@
+# Lin, M., Chen, Q., & Yan, S. (2013). Network in network. arXiv preprint arXiv:1312.4400.
+# Network in Network, Implemented MLPConv layer
 import torch
 from torch import nn
 
 
 class MLPConv(nn.Module):
-    def __init__(self, f_in, f_out):
+    def __init__(self, f_in, f_out, mlp_size):
         super(MLPConv, self).__init__()
-        self.layers = nn.Sequential([
-            nn.Conv2d(),
-            nn.ReLU(),
-            nn.MaxPool2d(),
-            nn.Linear(),
-            
-        ])
+        self.layers = nn.Sequential(
+            nn.Conv2d(f_in, f_out, 3, 1, 1),
+            nn.ReLU(inplace=True),
+        )
+        for _ in range(mlp_size):
+            self.layers.append(nn.Conv2d(f_out, f_out, 1, 1, 0))
+            self.layers.append(nn.ReLU(inplace=True))
+
+    def forward(self, x):
+        return self.layers(x)
+
+
+class NIN(nn.Module):
+    # Network in Network
+    # I didn't implemented the weight initialize
+    def __init__(self, num_classes):
+        super(NIN, self).__init__()
+        self.layers = nn.Sequential(  # I could not find the supplementary materials in the paper so the parameters is my setting
+            MLPConv(3, 192, 2),
+            nn.Dropout(0.5),
+            nn.MaxPool2d(3, 2, 1),
+            MLPConv(192, 192, 2),
+            nn.Dropout(0.5),
+            nn.MaxPool2d(3, 2, 1),
+            MLPConv(192, num_classes, 2),
+            nn.AdaptiveAvgPool2d(1),
+            nn.Flatten()
+        )
+
+    def forward(self, x):
+        return self.layers(x)
